e2665f084f62dc3c4c2b44f0b133e046f801928e
Ali Sindhi <64632604+MumtazAli123@users.noreply.github.com>
2023-02-26 01:01:48 +0500

    updaed

--- server/models/user.js
+++ server/models/user.js
@@ -1,30 +1,40 @@
 const mongoose = require("mongoose");
 
-const UserSchema = mongoose.Schema({
-    name: {
-        required: true,
-        type: String,
-        trim: true,
+const userSchema = mongoose.Schema({
+  name: {
+    required: true,
+    type: String,
+    trim: true,
+  },
+  email: {
+    required: true,
+    type: String,
+    trim: true,
+    Validate: {
+      Validator: (value) => {
+        const re =
+          /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        return value.match(re);
+      },
+      message: "Plz Enter Valid Email",
     },
-    email: {
-        required: true,
-        type: String,
-        trim: true,
-        Validate: {
-            Validator: (value)=>{
-                const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-                return value.match(re);
-            },
-            message: "Plz Enter Valid Email",
-        }
+  },
+  pass: {
+    required: true,
+    type: String,
+    Validate: {
+      Validator: (value) => {
+        return value.length > 6 ;
+      },
+      message: "Plz Enter Valid Email",
     },
-    pass: {
-        required: true,
-        type: String,
-    },
-    address: {
-        type: String,
-        
-    }
-
+  },
+  address: {
+    type: String,
+    default: "",
+  },
+  // cart
 });
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
