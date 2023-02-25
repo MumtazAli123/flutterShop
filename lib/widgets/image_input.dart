279430fe8ed01260cab2df15eb1c522fec6a2ae8
Ali Sindhi <64632604+MumtazAli123@users.noreply.github.com>
2023-02-26 03:50:49 +0500

    updated

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
+
 class ImageInputScreen extends StatefulWidget {
   const ImageInputScreen({Key? key}) : super(key: key);
 
@@ -10,26 +11,38 @@ class ImageInputScreen extends StatefulWidget {
 }
 
 class _ImageInputScreenState extends State<ImageInputScreen> {
-    late File _imageFile;
+  final urlImage = "https://imglarger.com/Images/before-after/ai-image-enlarger-1-after-2.jpg";
+  bool isFile = false;
+  File? _imageFile;
+  late Uint8List memoryImage;
+
+  Future getImage(ImageSource source) async {
+    final image = await ImagePicker().pickImage(source: source);
+    if (image == null) return;
+    final imageTemporary = File(image.path);
+    setState(() {
+      _imageFile = imageTemporary;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Row(
+    return Row(
       children: [
         Container(
           width: 200,
           height: 200,
-          decoration: BoxDecoration(
-            border: Border.all(width: 2, color: Colors.orange)
-          ),
+          decoration:
+              BoxDecoration(border: Border.all(width: 2, color: Colors.orange)),
           // ignore: unnecessary_null_comparison
-          child: _imageFile !=null?Image.file(_imageFile, fit: BoxFit.cover,): const Text('No Image Yet'),
+          child: _imageFile != null
+              ? Image.file(
+                  _imageFile!,
+                  fit: BoxFit.cover,
+                )
+              : const Text('No Image Yet'),
         )
       ],
     );
-  }
-  @override
-  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
-    super.debugFillProperties(properties);
-    properties.add(DiagnosticsProperty<File>('_imageFile', _imageFile));
   }
 }
